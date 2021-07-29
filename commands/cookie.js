8e769d55fac5e1d0f91f222c9288a586b18b129f
GreenChlorophyll <69466128+GreenChlorophyll@users.noreply.github.com>
2021-07-29 23:57:32 +0100

    Create cookie.js

---
+++ commands/cookie.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'cookie',
+	description: 'sends a cookie to a user.',
+	execute(message, args) {
+		message.channel.send('COOKIE TEST');
+	},
+};
+
+// This will have more code, just testing to make sure it works. Not too fimilar with this sort of system. 
