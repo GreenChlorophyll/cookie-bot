e80b830d952150234804dfd2591e4141e9f6d816
GreenChlorophyll <69466128+GreenChlorophyll@users.noreply.github.com>
2021-07-31 02:35:47 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,8 +6,8 @@ module.exports = {
 	execute(message, args) {
 		const cookiehelp = new Discord.MessageEmbed()
 		    .setColor('#D9A957')
-			.setTitle ('Help - Cookie Bot')
-			.setDescription('🍪 CookieBot is a bot that can send cookies to users. The bot is currently in beta-testing, more commands will be added. \n \n **=**cookie @user - Sends the user a cookie. \n **=**muffin @user - Sends the user a muffin.  \n **=**help - Sends the help embed. \n **=**info - Sends the info embed. \n \n Currently hosted on `ServerIvy` ')
+			.setTitle ('Help - BakeryBot')
+			.setDescription('🍪 BakeryBot is a bot that can send cookies to users. The bot is currently in beta-testing, more commands will be added. \n \n **=**cookie @user - Sends the user a cookie. \n **=**muffin @user - Sends the user a muffin.  \n **=**help - Sends the help embed. \n **=**info - Sends the info embed. \n \n Currently hosted on `ServerDuck` ')
 		message.channel.send(cookiehelp);
 	},
 };
