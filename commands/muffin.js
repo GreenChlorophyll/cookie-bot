a963e6c2a6aacfa31cc3808170ff09b71e90c2f7
GreenChlorophyll <69466128+GreenChlorophyll@users.noreply.github.com>
2021-07-31 02:29:38 +0100

    Update muffin.js

--- commands/muffin.js
+++ commands/muffin.js
@@ -1,1 +1,11 @@
-
+const Discord = require("discord.js")
+module.exports = {
+	name: 'muffin',
+	description: 'sends a muffin to a user.',
+	execute(message, args) {
+		const cookie = new Discord.MessageEmbed()
+		    .setColor('#D9A957')
+			  .setDescription(`${message.author} sent a freshly baked muffin to ${message.mentions.members.first()}`)
+		message.channel.send(cookie);
+	},
+};
