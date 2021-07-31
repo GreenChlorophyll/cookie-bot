bf4b1c6842f8e750865d2c08b2452e7efdc6bac1
GreenChlorophyll <69466128+GreenChlorophyll@users.noreply.github.com>
2021-07-31 02:36:18 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -5,8 +5,8 @@ module.exports = {
 	execute(message, args) {
 		const cookieinfo = new Discord.MessageEmbed()
 		    .setColor('#D9A957')
-			.setTitle ('Info - Cookie Bot')
-			.setDescription('🍪 CookieBot is a bot that can send cookies to users. The bot is currently in beta-testing, more commands will be added. \n \n Host: `ServerIvy` \n Verison: `0.1`')
+			.setTitle ('Info - BakeryBot')
+			.setDescription('🍪 BakeryBot is a bot that can send cookies to users. The bot is currently in beta-testing, more commands will be added. \n \n Host: `ServerDuck` \n Verison: `0.2.1`')
 		message.channel.send(cookieinfo);
 	},
 };
